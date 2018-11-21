27a4c4c9885d901b607ecef1bc4da3e90375c97e
Isaiah Islam <IIslam@nyc.yearup.org>
2018-11-21 11:57:14 -0500

    fixing Java

--- main.js
+++ main.js
@@ -1,48 +1,63 @@
 class Pokemon {
-  constructor (pokeImage, name, hp, attack, defense, ability) {
-    this.pokeImage = pokeImage;
+  constructor (name, hp, attack, defense, ability) {
     this.name = name;
     this.hp = hp;
     this.attack = attack;
     this.defense = defense;
     this.ability = ability;
-    this.container = document.querySelector('#pokeStats');
+    this.container = document.getElementById('pokemons');
     this.element = document.createElement("div");
     this.element.className = 'poke_products';
+  }
 
-    display () {
+  display(){
+    let name1 = document.getElementById('name1');
+    name1.innerHTML = "Name: " + this.name;
 
-  let pokeImage = document.getElementById('imgPlaceHolder');
-  pokeImage.src = this.pokeImage;
-  this.element.appendChild(pokeImage);
-  this.container.appendChild(this.element);
+    let hp1 = document.getElementById('HP1');
+    hp1.innerHTML = "HP: " + this.hp;
 
-  let name = document.createElement('h2');
-  name.innerHTML = "Name: " + this.name;
-  this.element.appendChild(name);
-  this.container.appendChild(this.element);
+    let attack1 = document.getElementById('Attack1');
+    attack1.innerHTML = "Attack: " + this.attack;
 
-  let hp = document.createElement('h3');
-  hp.innerHTML = "HP: " + this.hp;
-  this.element.appendChild(hp);
-  this.container.appendChild(this.element);
+    let defense1 = document.getElementById('Defense1');
+    defense1.innerHTML = "Defense: " + this.defense;
 
-  let attack = document.createElement('h3');
-  attack.innerHTML = "Attack: " + this.attack;
-  this.element.appendChild(attack);
-  this.container.appendChild(this.element);
+    let ability1 = document.getElementById('Ability1');
+    ability1.innerHTML = "Ability: " + this.ability;
 
-  let defense = document.createElement('h3');
-  defense.innerHTML = "Defense: " + this.defense;
-  this.element.appendChild(defense);
-  this.container.appendChild(this.element);
 
-  let ability = document.createElement('h3');
-  ability.innerHTML = "Ability: " + this.ability;
-  this.element.appendChild(ability);
-  this.container.appendChild(this.element);
+    let name2 = document.getElementById('name2');
+    name2.innerHTML = "Name: " + this.name;
 
-}
+    let hp2 = document.getElementById('HP2');
+    hp2.innerHTML = "HP: " + this.hp;
+
+    let attack2 = document.getElementById('Attack2');
+    attack2.innerHTML = "Attack: " + this.attack;
+
+    let defense2 = document.getElementById('Defense2');
+    defense2.innerHTML = "Defense: " + this.defense;
+
+    let ability2 = document.getElementById('Ability2');
+    ability2.innerHTML = "Ability: " + this.ability;
+
+
+    let name3 = document.getElementById('name3');
+    name3.innerHTML = "Name: " + this.name;
+
+    let hp3 = document.getElementById('HP3');
+    hp3.innerHTML = "HP: " + this.hp;
+
+    let attack3 = document.getElementById('Attack3');
+    attack3.innerHTML = "Attack: " + this.attack;
+
+    let defense3 = document.getElementById('Defense3');
+    defense3.innerHTML = "Defense: " + this.defense;
+
+    let ability3 = document.getElementById('Ability3');
+    ability3.innerHTML = "Ability: " + this.ability;
+  }
 }
 
 class Trainer {
@@ -63,24 +78,71 @@ class Trainer {
   }
 }
 
-function gallade(){
+let gallade_name;
+let gallade_hp;
+let gallade_attack;
+let gallade_defense;
+let gallade_ability;
 axios.get("http://fizal.me/pokeapi/api/v2/name/gallade.json")
-  .then(function(response) {
-    let gallade_img = "Gallade.png";
-    let gallade_name = "Gallade";
-    let gallade_hp = response.data.stats[5].base_stat;
-    let gallade_attack = response.data.stats[4].base_stat;
-    let gallade_defense = response.data.stats[3].base_stat;
+  .then(gallade)
+  function gallade(response){
+    gallade_name = response.data.species.name;
+    gallade_hp = response.data.stats[5].base_stat;
+    gallade_attack = response.data.stats[4].base_stat;
+    gallade_defense = response.data.stats[3].base_stat;
+    gallade_ability = response.data.abilities[0].ability.name
+    console.log(response.data);
+    console.log(gallade_name);
+    console.log(gallade_attack);
+  }
 
-    let gallade_ability = response.data.abilities[0].ability.name;
-    console.log(gallade_ability);
-    for (let i = 0; i < response.data.abilities[i].ability.length; i++) {
-      let result = response.data.abilities[i].ability.name;
-    }
+let lucario_name;
+let lucario_hp;
+let lucario_attack;
+let lucario_defense;
+let lucario_ability;
+axios.get("http://fizal.me/pokeapi/api/v2/name/lucario.json")
+  .then(lucario)
+  function lucario(response){
+    lucario_name = response.data.species.name;
+    lucario_hp = response.data.stats[5].base_stat;
+    lucario_attack = response.data.stats[4].base_stat;
+    lucario_defense = response.data.stats[3].base_stat;
+    lucario_ability = response.data.abilities[0].ability.name
+    console.log(response.data);
+    console.log(lucario_name);
+    console.log(lucario_attack);
+  }
+
+let absol_name;
+let absol_hp;
+let absol_attack
+let absol_defense
+let absol_ability
+axios.get("http://fizal.me/pokeapi/api/v2/name/absol.json")
+  .then(absol)
+  function absol(response){
+    absol_name = response.data.species.name;
+    absol_hp = response.data.stats[5].base_stat;
+    absol_attack = response.data.stats[4].base_stat;
+    absol_defense = response.data.stats[3].base_stat;
+    absol_ability = response.data.abilities[0].ability.name
+    console.log(response.data);
+    console.log(absol_name);
+    console.log(absol_attack);
+  }
+
+    // let gallade_ability = response.data.abilities[0].ability.name;
+    // console.log(gallade_ability);
+    // for (let i = 0; i < response.data.abilities[i].ability.length; i++)
+    //   let result = response.data.abilities[i].ability.name;
 
 
-    gallade = new Pokemon(gallade_img, gallade_name, gallade_hp, gallade_attack, gallade_defense, gallade_ability,);
+    gallade = new Pokemon(gallade_name, gallade_hp, gallade_attack, gallade_defense, gallade_ability,);
     gallade.display();
 
-  })
-}
+    lucario = new Pokemon(lucario_name, lucario_hp, lucario_attack, lucario_defense, lucario_ability,);
+    lucario.display();
+
+    absol = new Pokemon(absol_name, absol_hp, absol_attack, absol_defense, absol_ability,);
+    absol.display();
